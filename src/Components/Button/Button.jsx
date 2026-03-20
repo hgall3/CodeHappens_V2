e564c5d0b04421fc688b2b8be8bcf5649ef4c58e
Gabi <gagallegosanda@gmail.com>
2026-03-20 18:48:43 +0100

    Feat: Button component created

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import './Button.scss';
+
+const Button = ({ text, onClick, type = "button", className = "" }) => {
+  return (
+    <button 
+      type={type} 
+      className={`custom-button ${className}`} 
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
